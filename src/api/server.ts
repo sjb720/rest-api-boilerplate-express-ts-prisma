8f74cbcd586d5d51032d2acc010d90b1a4f5a83d
Stephen Buonamassa <sbuonama@umd.edu>
2023-04-14 19:45:39 -0400

    added simple middleware rate limiter

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,6 +3,7 @@ import express, { json, Request, Response, NextFunction } from 'express';
 import Logger from '../config/logger';
 import { UserRoutes } from './components/user/routes';
 import { BookRoutes } from './components/book/routes';
+import rateLimit from 'express-rate-limit'
 
 export class Server {
 	private readonly _app: express.Application = express();
@@ -19,10 +20,22 @@ export class Server {
 
 	private registerMiddleware() {
 		this._app.use(json());
+
+		// Setup default limiting
+		const limiter = rateLimit({
+			windowMs: 15 * 60 * 1000, // 15 minutes
+			max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+			standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+			legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+		})
+		
+		// Apply the rate limiting middleware to all requests
+		this._app.use(limiter)
+		
 	}
 
 	private registerRoutes() {
-		this._app.get('/', (req: Request, res: Response) => res.send('Ahoy Friend!'));
+		this._app.get('/', (req: Request, res: Response) => res.send('Hi Friend.'));
 		this._app.use('/users', new UserRoutes().router);
 		this._app.use('/books', new BookRoutes().router);
 
